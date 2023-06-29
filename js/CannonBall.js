fae3a1fc7b52378985bee906591476d476f8c9a3
laizamonique <133267385+laizamonique@users.noreply.github.com>
2023-06-29 16:24:26 -0300

    Update CannonBall.js

--- js/CannonBall.js
+++ js/CannonBall.js
@@ -8,7 +8,15 @@ class CannonBall {
     this.image = loadImage("./assets/cannonball.png");
     World.add(world, this.body);
   }
-
+shoot() {
+var newAngle = cannon.angle - 28;
+newAngle = newAngle *(3.14/180)
+var velocity = p5.Vector.fromAngle(newAngle);
+velocity.mult(0.5);
+Matter.Body.setStatic(this.body, false);
+Matter.Body.setVelocity(this.body, {
+x: velocity.x *(180/3.14), y: velocity.y * (180/3.14)});
+}
 
   display() 
   {
